1a459930b548c5050afb9bc1472139c2122e1c18
Ilya198412 <110129197+Ilya198412@users.noreply.github.com>
2022-11-10 09:51:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,5 +4,5 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
-bot = Bot(token='5687686512:AAF4m20rqMR2tOPhvxQ6rRgYStUgboF_F4g')
+bot = Bot(token='----------')
 dp = Dispatcher(bot)
